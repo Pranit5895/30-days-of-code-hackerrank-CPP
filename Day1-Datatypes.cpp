8372f83a5a346fbc5a4b5d30834191bacff147b6
Pranit5895 <guptapranit9@gmail.com>
2020-04-20 12:47:09 -0700

    Day 1 code added

---
+++ Day1-Datatypes.cpp
@@ -0,0 +1,42 @@
+
+
+    int a;
+    double b;
+    string c;
+
+    cin>>a;
+    cin>>b;
+    getline(cin,c);
+    getline(cin,c);
+
+    int sumi=0; 
+    double sumf=0;
+    
+    sumi=i+a;
+    cout<<sumi<<"\n";
+   
+    
+    sumf=d+b;
+
+    cout<<fixed<<setprecision(1)<<sumf<<"\n";
+    
+    s=s+c;
+    cout<<s;
+    
+
+
+    
+    // Declare second integer, double, and String variables.
+    
+    // Read and save an integer, double, and String to your variables.
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    
+    // Print the sum of both integer variables on a new line.
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+
+
+
