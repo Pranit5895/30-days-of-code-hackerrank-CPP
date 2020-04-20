307a7d4abc544e071a17e2dce22f2ca10c7a2110
Pranit5895 <guptapranit9@gmail.com>
2020-04-20 12:48:03 -0700

    Day 2 code added

---
+++ Day2-Operators.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+  // Enter your code here. Read input from STDIN. Print output to STDOUT
+
+  double mealCost;
+  int tipPercent;
+  int taxPercent;
+
+  cin >> mealCost;
+  cin >> tipPercent;
+  cin >> taxPercent;
+
+  double cost = mealCost * (1 + (tipPercent + taxPercent) / 100.0);
+
+  double totalCost = round(cost);
+
+  // Print your result
+  cout << totalCost;
+
+  return 0;
+}
+
