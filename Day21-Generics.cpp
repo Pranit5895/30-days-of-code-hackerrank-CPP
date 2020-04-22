18d189fc57da5e82411f4f418bf0afa92bc00dce
Pranit5895 <guptapranit9@gmail.com>
2020-04-22 12:08:42 -0700

    Day 21 code added

---
+++ Day21-Generics.cpp
@@ -0,0 +1,18 @@
+
+template <typename t>
+void printArray (vector<t> v_) {
+    for (auto &element : v_) {
+        cout << element << endl;
+    }
+}
+
+
+/**
+*    Name: printArray
+*    Print each element of the generic vector on a new line. Do not return anything.
+*    @param A generic vector
+**/
+
+// Write your code here
+
+
